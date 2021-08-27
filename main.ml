939d4755ad444ad6233e75b17f7af41f08d01802
bynect <68197565+bynect@users.noreply.github.com>
2021-08-28 00:30:58 +0200

    Refactor main.ml

--- main.ml
+++ main.ml
@@ -5,29 +5,21 @@ let () =
                 Let("y", Var("m"),
                     Let("x", App(Var("y"), Lit(Bool true)),
                     Var("x"))))
-    (*let expr = Let("id", Abs("x", Var "x"),
-                Let("_", App(Var("id"), Var("id")),
-                    App(Var("id"), Lit(Bool true))))
-                    *)
     in
-    let (ty, a, c) = collect Set.empty e
-    in
-    print_endline "Assumptions";
-    List.iter (fun a' -> Print.string_of_assump a' |> print_endline) a;
-    print_endline "Constraints";
-    List.iter (fun c' -> Print.string_of_constr c' |> print_endline) c;
-    let s = solve c in
-    print_endline "Substitutions";
-    Print.string_of_subst s |> print_endline;
-    let ty' = apply s ty
-    in
-    print_endline "Type";
-    Printf.printf "%s => %s\n" (Print.string_of_ty ty) (Print.string_of_ty ty');
-    let (s, ty) = infer Map.empty e in
-    print_endline "Substitutions";
-    Print.string_of_subst s |> print_endline;
-    print_endline "Type";
-    Printf.printf "%s\n" (Print.string_of_ty ty)
+    try
+        let (s, ty) = infer Map.empty e in
+            print_endline "Substitutions";
+            Print.string_of_subst s |> print_endline;
+            print_endline "Type";
+            Print.string_of_ty ty |> print_endline
+    with
+    | Error(UnboundVar var) -> Printf.printf "Unbound variable `%s`" var
+    | Error(OccurFail(var, ty)) ->
+            Printf.printf "Occurs check failed in `%s = %s`"
+            var (Print.string_of_ty ty)
+    | Error(UnifyFail(t1, t2)) ->
+            Printf.printf "Unification failed in `%s = %s`"
+            (Print.string_of_ty t1) (Print.string_of_ty t2)
 
 
 
