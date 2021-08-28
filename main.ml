4a5d4d7a32d8e2fb27187f9056ef97bcdb44c349
bynect <68197565+bynect@users.noreply.github.com>
2021-08-29 00:32:18 +0200

    Normalize types when reporting errors

--- main.ml
+++ main.ml
@@ -1,6 +1,6 @@
 open Infer
 
-let normalize (ty : ty) : scheme =
+let normalize (ty : ty) : subst * scheme =
   let letter (i : int) : var =
     let off, count = (i mod 26, i / 26) in
     let chr = Char.code 'a' + off |> Char.chr in
@@ -10,7 +10,11 @@ let normalize (ty : ty) : scheme =
   let vars' = List.mapi (fun i _ -> letter i) vars in
   let s = List.fold_left2 (fun acc var var' -> Map.add var (TVar(var')) acc)
     Map.empty vars vars' in
-  Scheme(vars', apply s ty)
+  s, Scheme(vars', apply s ty)
+
+let normalize' (ty : ty) : scheme =
+    let _, sigma = normalize ty in
+    sigma
 
 let main (gamma : env) =
   while true do
@@ -25,18 +29,24 @@ let main (gamma : env) =
         print_string "ty = ";
         Pretty.string_of_ty ty |> print_endline;
         print_string "scheme = ";
-        normalize ty |> Pretty.string_of_scheme |> print_endline
+        normalize' ty |> Pretty.string_of_scheme |> print_endline
     with
     | Lex.Lex_error -> print_endline "Error while lexing"
     | Parsing.Parse_error -> print_endline "Error while parsing"
     | Infer.Error (UnboundVar var) ->
-        Printf.printf "Unbound variable `%s`\n" var
+        Printf.printf "Unbound variable %s\n" var
     | Infer.Error (OccurFail (var, ty)) ->
-        Printf.printf "Occurs check failed in `%s = %s`\n" var
-          (Pretty.string_of_ty ty)
+        let s, Scheme(_, ty') = normalize ty in
+        Printf.printf "Occurs check failed in %s = %s\n"
+        (match Map.find_opt var s with
+        | Some(TVar(var')) -> var'
+        | _ -> failwith "Unreachable")
+          (Pretty.string_of_ty ty')
     | Infer.Error (UnifyFail (t1, t2)) ->
-        Printf.printf "Unification failed in `%s = %s`\n"
-          (Pretty.string_of_ty t1) (Pretty.string_of_ty t2)
+        let Scheme(_, t1') = normalize' t1
+        and Scheme(_, t2') = normalize' t2 in
+        Printf.printf "Unification failed in %s = %s\n"
+          (Pretty.string_of_ty t1') (Pretty.string_of_ty t2')
   done
 
 let () =
