9cf23c9d58b5b73391ff8b218b400b9d53fe83b9
bynect <68197565+bynect@users.noreply.github.com>
2021-08-28 11:27:24 +0200

    Rename print.ml to pretty.ml

--- main.ml
+++ main.ml
@@ -4,17 +4,17 @@ let main e =
     try
         let (s, ty) = infer Map.empty e in
             print_endline "Substitutions";
-            Print.string_of_subst s |> print_endline;
+            Pretty.string_of_subst s |> print_endline;
             print_endline "Type";
-            Print.string_of_ty ty |> print_endline
+            Pretty.string_of_ty ty |> print_endline
     with
     | Error(UnboundVar var) -> Printf.printf "Unbound variable `%s`" var
     | Error(OccurFail(var, ty)) ->
             Printf.printf "Occurs check failed in `%s = %s`"
-            var (Print.string_of_ty ty)
+            var (Pretty.string_of_ty ty)
     | Error(UnifyFail(t1, t2)) ->
             Printf.printf "Unification failed in `%s = %s`"
-            (Print.string_of_ty t1) (Print.string_of_ty t2)
+            (Pretty.string_of_ty t1) (Pretty.string_of_ty t2)
 
 
 
